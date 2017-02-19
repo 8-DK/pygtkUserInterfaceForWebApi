22b7eb11757066d4fe45db8e3aa35ce681bc39f4
DK <khairnardm@gmail.com>
2017-02-19 12:49:49 +0530

    get user info and store in xls file in tmp folder

--- main.py
+++ main.py
@@ -4,8 +4,8 @@
 import gi
 import json
 import urllib2
-
 import os
+import xlwt
 from urlparse import urlparse
 from os.path import splitext, basename
 
@@ -20,15 +20,27 @@ except:
 	pass
 
 
+ApiUrlGetUserInfo = "https://v2api.collaborizm.com/v2/users/21339"
+ApiUrlGetUserProfileImage = "https://res.cloudinary.com/collabizm/image/facebook/c_fill,w_200,h_200,q_auto,g_face,dpr_1,f_auto/v1/"
+ApiUrlGetUserList = "https://v2api.collaborizm.com/v2/people/list?page=" #page=0 get page 0 each page contain 16 user
+
 
 class Handler:
 	def onDeleteWindow(self, *args):
 		Gtk.main_quit(*args)
 
+	def userSelectFronList(self, *args):	  
+		treeViewWidget = args[0]
+		selectionId = args[1]
+		treeViewWidgetColumn = args[2]
+		selection = treeViewWidget.get_selection()
+		model, treeiter = selection.get_selected()
+		if treeiter != None:
+			print("You selected", model[treeiter][0][3])
+	
+
 class mainGtkClass:
 	def __init__(self):
-		self.ApiUrlGetUserBaseUserInfo = "https://res.cloudinary.com/collabizm/image/facebook/c_fill,w_200,h_200,q_auto,g_face,dpr_1,f_auto/v1/"
-		self.apiUrlGetUserList = "https://v2api.collaborizm.com/v2/people/list?page=" #page=0 get page 0 each page contain 16 user
 		builder = Gtk.Builder()
 		builder.add_from_file("main.glade")	
 		builder.connect_signals(Handler())
@@ -51,102 +63,73 @@ class mainGtkClass:
 		#self.Tab2View = builder.get_object("Tab2View")
 		#self.Tab3View = builder.get_object("Tab3View")
 		
-		self.window.maximize()
-
-		self.liststore_project.append(["Tiny Wifi Server",1000,1])
-		self.liststore_project.append(["TIme Pass",0,0])
-		self.liststore_project.append(["sfdads",56,0])
+		self.window.maximize()		
 
 		renderer = Gtk.CellRendererText()
-		column = Gtk.TreeViewColumn("Project", renderer, text=0)
-		self.treeview_project.append_column(column)
+		self.column1 = Gtk.TreeViewColumn("User", renderer, text=0)
 
-		column = Gtk.TreeViewColumn("Like", renderer, text=1)
-		self.treeview_project.append_column(column)
+		self.treeview_project.append_column(self.column1)
+
+		self.column2 = Gtk.TreeViewColumn("Like", renderer, text=1)
+		self.treeview_project.append_column(self.column2)
 	
-		column = Gtk.TreeViewColumn("Status", renderer, text=2)
-		self.treeview_project.append_column(column)
-		
-		url = "https://v2api.collaborizm.com/v2/users/21339"
-		response = urllib2.urlopen(url)
+		self.column3 = Gtk.TreeViewColumn("Status", renderer, text=2)
+		self.treeview_project.append_column(self.column3)
+		#self.column1.set_sort_column_id(0) #sort column by name
+		#create excel file for user list and ID
+		self.book = xlwt.Workbook(encoding="utf-8")
+		self.sheet1 = self.book.add_sheet("Sheet 1",cell_overwrite_ok=True)
+	
+		response = urllib2.urlopen(ApiUrlGetUserInfo)
 		data = json.loads(response.read())
+		self.setUserName(data)				
 
-		self.setUserName(data)		
+		self.getUserList();
+
 		self.window.show_all()
+
+	def getUserList(self):
+		
+		index = 0
+		response = urllib2.urlopen(ApiUrlGetUserList+str(0))
+		jsonObj = json.loads(response.read())
+		
+
+		#for item in jsonObj:
+		for index in range(len(jsonObj)):
+			fullname = jsonObj[index]['first_name']+" "+jsonObj[index]['last_name']
+			userId = jsonObj[index]['id']
+			userAvtarId = jsonObj[index]['facebook_id']
+			print userId+"->"+fullname+"->"+userAvtarId
+			#store data in excel file 
+			self.sheet1.write(index, 0, userId)
+			self.sheet1.write(index, 1, fullname)
+			self.sheet1.write(index, 2, userAvtarId)
+			self.book.save("/tmp/userlist.xls")
+
+			self.liststore_project.append([fullname,0,1,userId])	#add full name to list view
+			index = index+1
+		
 
 	def setUserName(self,jsonObj):
 		value = jsonObj["first_name"]+" "
 		value += jsonObj["last_name"]
-		self.usernameLable.set_text(value)
+		self.usernameLable.set_text(value)	#top user name lable
 		lbl = Gtk.Label(value)
 		lbl.set_alignment(xalign=0, yalign=1) 
 
 		lbl1 = Gtk.Label(value)
 		lbl1.set_alignment(xalign=0, yalign=1) 
-		lbl2 = Gtk.Label(value)
-		lbl2.set_alignment(xalign=0, yalign=1) 
-		lbl3 = Gtk.Label(value)
-		lbl3.set_alignment(xalign=0, yalign=1) 
-		lbl4 = Gtk.Label(value)
-		lbl4.set_alignment(xalign=0, yalign=1) 
-		lbl5 = Gtk.Label(value)
-		lbl5.set_alignment(xalign=0, yalign=1) 
-		lbl6 = Gtk.Label(value)
-		lbl6.set_alignment(xalign=0, yalign=1) 
-		lbl7 = Gtk.Label(value)
-		lbl7.set_alignment(xalign=0, yalign=1) 
-		lbl8 = Gtk.Label(value)
-		lbl8.set_alignment(xalign=0, yalign=1) 
-		lbl9 = Gtk.Label(value)
-		lbl9.set_alignment(xalign=0, yalign=1) 
-		lbl10 = Gtk.Label(value)
-		lbl10.set_alignment(xalign=0, yalign=1) 
-		lbl11 = Gtk.Label(value)
-		lbl11.set_alignment(xalign=0, yalign=1) 
-		lbl12 = Gtk.Label(value)
-		lbl12.set_alignment(xalign=0, yalign=1) 
-		lbl13 = Gtk.Label(value)
-		lbl13.set_alignment(xalign=0, yalign=1) 
-		lbl14 = Gtk.Label(value)
-		lbl14.set_alignment(xalign=0, yalign=1) 
-		lbl15 = Gtk.Label(value)
-		lbl15.set_alignment(xalign=0, yalign=1) 
-		lbl16 = Gtk.Label(value)
-		lbl16.set_alignment(xalign=0, yalign=1) 
-		lbl17 = Gtk.Label(value)
-		lbl17.set_alignment(xalign=0, yalign=1) 
-		lbl18 = Gtk.Label(value)
-		lbl18.set_alignment(xalign=0, yalign=1) 
-		lbl19 = Gtk.Label(value)
-		lbl19.set_alignment(xalign=0, yalign=1) 
-		
 
 		self.Tab1ListBox.add(lbl)
-		self.Tab1ListBox.add(lbl1)
-		self.Tab1ListBox.add(lbl2)
-		self.Tab1ListBox.add(lbl3)
-		self.Tab1ListBox.add(lbl4)
-		self.Tab1ListBox.add(lbl5)
-		self.Tab1ListBox.add(lbl6)
-		self.Tab1ListBox.add(lbl7)
-		self.Tab1ListBox.add(lbl8)
-		self.Tab1ListBox.add(lbl9)
-		self.Tab1ListBox.add(lbl10)
-		self.Tab1ListBox.add(lbl11)
-		self.Tab1ListBox.add(lbl12)
-		self.Tab1ListBox.add(lbl13)
-		self.Tab1ListBox.add(lbl14)
-		self.Tab1ListBox.add(lbl15)
-		self.Tab1ListBox.add(lbl16)
-		self.Tab1ListBox.add(lbl17)
-		self.Tab1ListBox.add(lbl18)
-		self.Tab1ListBox.add(lbl19)
 
-		self.downlodImage(jsonObj["facebook_id"],"/tmp/")
 
-	def downlodImage(self,imageUrl,path):
-		
-		fullUrl	= self.ApiUrlGetUserBaseUserInfo + imageUrl
+		self.downlodImage(jsonObj)
+
+	def downlodImage(self,jsonObj):	#Download User Profile image
+		imageUrl = jsonObj["facebook_id"]
+		path = "/tmp/"
+		fullUrl	= ApiUrlGetUserProfileImage + imageUrl
 		disassembled = urlparse(imageUrl)
 		imageName = basename(disassembled.path)
 		path+=imageName+".jpg"
